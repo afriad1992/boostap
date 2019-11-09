e3336a072f03197a172c3c8a313e14ae60f58324
afriad1992 <56979262+afriad1992@users.noreply.github.com>
2019-11-09 20:16:48 +0100

    Create scratchpad.js

---
+++ assets/img/scratchpad.js
@@ -0,0 +1,31 @@
+var phrase=prompt("saisir une phrase");
+var lettre=prompt("saisir une lettre");
+var n=0;;
+for (i=0;i<=phrase.length;i++)
+{if(phrase[i]==lettre){n++;}}
+
+/*
+Exception: TypeError: phrase is null
+@Scratchpad/1:4:10
+@Scratchpad/1:1:12
+@Scratchpad/1:1:12
+@Scratchpad/1:1:12
+@Scratchpad/1:1:12
+@Scratchpad/1:1:12
+@Scratchpad/1:1:12
+@Scratchpad/1:1:12
+*/
+/*
+Exception: TypeError: phrase is null
+@Scratchpad/1:4:10
+@Scratchpad/1:1:12
+@Scratchpad/1:1:12
+@Scratchpad/1:1:12
+@Scratchpad/1:1:12
+@Scratchpad/1:1:12
+@Scratchpad/1:1:12
+*/
+/*
+Exception: TypeError: phrase is null
+@Scratchpad/1:4:10
+*/
